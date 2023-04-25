c45fefcc7abe44a5573b2ae92f5a65250c43bc7b
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-04-25 10:36:21 -0400

    Update 1_export.py

--- pages/1_export.py
+++ pages/1_export.py
@@ -112,11 +112,11 @@ if sub1:
     col1, col2 = st.columns(2)
     with col1:
        st.subheader("Formato claro")
-       st.image(path+"OPCION1.png")
+       st.image("OPCION1.png")
        
     with col2:
        st.subheader("Formato oscuro")
-       st.image(path+"OPCION2.png")
+       st.image("OPCION2.png")
     
    
     
