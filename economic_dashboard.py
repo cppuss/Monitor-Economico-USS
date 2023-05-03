7e987ed47257fbad4b984195b8059c00f578e406
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-05-03 17:23:57 -0400

    Update economic_dashboard.py

--- economic_dashboard.py
+++ economic_dashboard.py
@@ -122,7 +122,7 @@ def to_excel(df):
     worksheet = writer.sheets['Sheet1']
     format1 = workbook.add_format({'num_format': '0.00'}) 
     worksheet.set_column('A:A', None, format1)  
-    writer.save(binary_output)
+    writer.save()
     processed_data = output.getvalue()
     return processed_data
 
