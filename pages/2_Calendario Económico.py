2c2403c4066b76b7a9c8569552e59fda8a37fe46
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-05-04 11:45:35 -0400

    Create 2_Calendario Económico.py

---
+++ pages/2_Calendario Económico.py
@@ -0,0 +1,7 @@
+
+
+st.markdown("<h1 style='text-align: center; color: black;'>MONITOR ECONÓMICO CPP USS</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: grey;'>Generar presentación con información económica</h2>", unsafe_allow_html=True)
+
+
+st.write(' ')
