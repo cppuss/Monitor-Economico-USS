9739ba8b57d5fd131fbcf65e6ed2c15ee4f019cd
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-06-07 11:56:23 -0400

    Update 2_Calendario Económico.py

--- pages/2_Calendario Económico.py
+++ pages/2_Calendario Económico.py
@@ -10,7 +10,7 @@ from datetime import date
 from pptx.dml.color import RGBColor
 from pptx.util import Inches
 import os
-
+from datetime import datetime
 st.markdown("<h1 style='text-align: center; color: black;'>MONITOR ECONÓMICO CPP USS</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center; color: grey;'>Fechas de publicación información económica</h2>", unsafe_allow_html=True)
 
@@ -22,7 +22,17 @@ st.write(' ')
 st.sidebar.image("ESCUDOUSS_vertical_color.png", use_column_width=True)
 #st.image("calendario.png")
 
+
+data=pd.read_parquet("datos_monitor.parquet")
+
+
+data1=data[data["CATEGORIA"]=="ACTIVIDAD ECONOMICA"]
+
+extremos_1=[data1["PERIODO"].iloc[0].to_pydatetime(),datetime.now()]
+
+
+
 appointment = st.slider(
-        "Seleccione el rango de fechas",
-        value=(2020,2022),
-        format="YYYY/MM")
+                "Seleccione el rango de fechas",
+                value=(extremos_1[0],extremos_1[1]),
+                format="YYYY/MM")
