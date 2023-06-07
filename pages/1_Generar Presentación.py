65d1509dc0dda8337305898c37ebea32622ac446
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-06-07 15:15:57 -0400

    Update 1_Generar Presentación.py

--- pages/1_Generar Presentación.py
+++ pages/1_Generar Presentación.py
@@ -423,15 +423,15 @@ if sub1:
                 os.remove("comp.png")
             except:
                 pass
-            try:
-                slide2 = prs.slides[2]
-                texto = "IMACEC anotó un valor de :" +str(ultimo_valor) 
-                title_2 = slide2.shapes.title.text_frame.paragraphs[0]
-                title_2.text = texto
-                title_2.font.color.rgb = RGBColor(0, 0, 0)  # Color blanco
-                title_2.font.bold = False  # Sin negrita
-            except:
-                pass
+         #   try:
+         #       slide2 = prs.slides[2]
+         #       texto = "IMACEC anotó un valor de :" +str(ultimo_valor) 
+         #       title_2 = slide2.shapes.title.text_frame.paragraphs[0]
+         #       title_2.text = texto
+         #       title_2.font.color.rgb = RGBColor(0, 0, 0)  # Color blanco
+         #       title_2.font.bold = False  # Sin negrita
+         #   except:
+         #       pass
             
             try:
                 #SLIDE 3 
