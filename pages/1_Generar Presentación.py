b07b679cb9552f6d1e1ac74b6558e4a607320f75
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-06-07 22:59:42 -0400

    Update 1_Generar Presentación.py

--- pages/1_Generar Presentación.py
+++ pages/1_Generar Presentación.py
@@ -471,8 +471,8 @@ if sub1:
             except:
                 pass
                 
-                mayor = np.max(np.abs([uv_servicios, uv_bienes, uv_alimentos, uv_energia, uv_volatiles]))
-                etiqueta = np.argmax(np.abs([uv_servicios, uv_bienes, uv_alimentos, uv_energia, uv_volatiles]))      
+                mayor = np.max(np.fabs([uv_servicios, uv_bienes, uv_alimentos, uv_energia, uv_volatiles]))
+                etiqueta = np.argmax(np.fabs([uv_servicios, uv_bienes, uv_alimentos, uv_energia, uv_volatiles]))
                 etiquetas={0:"Servicios no volátiles",
                     1:"Bienes no volátiles",
                     2:"Alimentos",
