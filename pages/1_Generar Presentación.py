f66c90e052ab41d3cd94de54c4451bc068efadc9
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-06-07 23:21:26 -0400

    Update 1_Generar Presentación.py

--- pages/1_Generar Presentación.py
+++ pages/1_Generar Presentación.py
@@ -292,7 +292,7 @@ if sub1:
                 inf_anu["SERIE"]=inf_anu["NOMBRE_2"]
                 uv_inf=inf_anu["VALOR"].loc[-1]
                 
-                inf_anu1=inf_anu.copy(deep=True)
+              
                 inf_anu=gen(inf_anu,appointment_2,"Variación porcentual IPC YoY")
                 inf_anu=eje_porcentaje(inf_anu)
 
@@ -300,11 +300,11 @@ if sub1:
                 com_anu["SERIE"]=com_anu["NOMBRE_2"]
                 comp_2=com_anu[~com_anu["SERIE"].isin(["IPC sin volátiles","IPC volátil"])]
                
-                uv_servicios=comp_2[comp_2['NOMBRE_2'] == "IPC Servicios sin volátiles"]['VALOR'].loc[-1]
-                uv_bienes=comp_2[comp_2['NOMBRE_2'] == "IPC Bienes sin volátiles"]['VALOR'].loc[-1]
-                uv_alimentos=comp_2[comp_2['NOMBRE_2'] == "IPC Alimentos volátiles"]['VALOR'].loc[-1]
-                uv_energia=comp_2[comp_2['NOMBRE_2'] == "IPC Energía volátiles"]['VALOR'].loc[-1]
-                uv_volatiles=comp_2[comp_2['NOMBRE_2'] == "IPC Resto de volátiles"]['VALOR'].loc[-1]
+                uv_servicios=com_anu[com_anu['NOMBRE_2'] == "IPC Servicios sin volátiles"]['VALOR'].loc[-1]
+                uv_bienes=com_anu[com_anu['NOMBRE_2'] == "IPC Bienes sin volátiles"]['VALOR'].loc[-1]
+                uv_alimentos=com_anu[com_anu['NOMBRE_2'] == "IPC Alimentos volátiles"]['VALOR'].loc[-1]
+                uv_energia=com_anu[com_anu['NOMBRE_2'] == "IPC Energía volátiles"]['VALOR'].loc[-1]
+                uv_volatiles=com_anu[com_anu['NOMBRE_2'] == "IPC Resto de volátiles"]['VALOR'].loc[-1]
                   
                 comp_2.loc[comp_2['NOMBRE_2'] == "IPC Servicios sin volátiles", 'VALOR'] = comp_2.loc[comp_2['NOMBRE_2'] == "IPC Servicios sin volátiles", 'VALOR']*0.384
                 comp_2.loc[comp_2['NOMBRE_2'] == "IPC Bienes sin volátiles", 'VALOR'] = comp_2.loc[comp_2['NOMBRE_2'] == "IPC Bienes sin volátiles", 'VALOR']*0.267
@@ -468,14 +468,14 @@ if sub1:
             except:
                 pass
 
-            mayor = np.fabs([uv_servicios, uv_bienes, uv_alimentos, uv_energia, uv_volatiles])
-            mayor = np.max(np.abs([uv_servicios, uv_bienes, uv_alimentos, uv_energia, uv_volatiles]))
-            etiqueta = np.argmax(np.fabs([uv_servicios, uv_bienes, uv_alimentos, uv_energia, uv_volatiles]))
-            etiquetas={0:"Servicios no volátiles",
-                    1:"Bienes no volátiles",
-                    2:"Alimentos",
-                    3:"Energía",
-                    4:"Resto de volátiles"}
+         #   mayor = np.fabs([uv_servicios, uv_bienes, uv_alimentos, uv_energia, uv_volatiles])
+         #   mayor = np.max(np.abs([uv_servicios, uv_bienes, uv_alimentos, uv_energia, uv_volatiles]))
+         #   etiqueta = np.argmax(np.fabs([uv_servicios, uv_bienes, uv_alimentos, uv_energia, uv_volatiles]))
+         #   etiquetas={0:"Servicios no volátiles",
+         #           1:"Bienes no volátiles",
+         #           2:"Alimentos",
+         #           3:"Energía",
+         #           4:"Resto de volátiles"}
                 
             try:  
                 slide2 = prs.slides[5]
