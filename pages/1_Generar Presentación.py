b85c7f10cfa36cb6551b492841e0d3d0527e3990
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-06-07 23:24:43 -0400

    Update 1_Generar Presentación.py

--- pages/1_Generar Presentación.py
+++ pages/1_Generar Presentación.py
@@ -290,7 +290,7 @@ if sub1:
                 anu="IPC, IPC sin volátiles e IPC volátiles, variación anual, información empalmada"
                 inf_anu=data2[(data2["NOMBRE_1"]==anu)&(data2["NOMBRE_2"]=="IPC General")]
                 inf_anu["SERIE"]=inf_anu["NOMBRE_2"]
-                #uv_inf=inf_anu["VALOR"].loc[-1]
+                uv_inf=inf_anu["VALOR"].loc[-1]
                 
               
                 inf_anu=gen(inf_anu,appointment_2,"Variación porcentual IPC YoY")
