6c683366d579cbbdbaa1c6fbc6e70761d9b3f5b7
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-06-13 12:58:28 -0400

    Update 2_Calendario Económico.py

--- pages/2_Calendario Económico.py
+++ pages/2_Calendario Económico.py
@@ -24,27 +24,37 @@ st.sidebar.image("ESCUDOUSS_vertical_color.png", use_column_width=True)
 
 
 data=pd.read_parquet("datos_monitor.parquet")
+
 import plotly.graph_objects as go
-x = [1, 2, 3, 4, 5]
+from datetime import datetime
+
+# Crear los datos de muestra para el gráfico
+x = [
+    datetime(2023, 1, 1),
+    datetime(2023, 2, 1),
+    datetime(2023, 3, 1),
+    datetime(2023, 4, 1),
+    datetime(2023, 5, 1)
+]
 y = [10, 20, 30, 40, 50]
 
-# Diccionario de traducción para los números en español
-diccionario_numeros = {
-    1: 'uno',
-    2: 'dos',
-    3: 'tres',
-    4: 'cuatro',
-    5: 'cinco'
+# Diccionario de traducción para los meses en español
+diccionario_meses = {
+    1: 'enero',
+    2: 'febrero',
+    3: 'marzo',
+    4: 'abril',
+    5: 'mayo'
 }
 
-# Convertir los números del eje x a su versión en español
-x_labels = [diccionario_numeros[num] for num in x]
+# Convertir las fechas del eje x a su versión en español
+x_labels = [f"{diccionario_meses[date.month]} {date.year}" for date in x]
 
 # Crear la figura de Plotly con los ejes en español
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=x, y=y, mode='lines'))
 fig.update_layout(
-    xaxis_title='Eje X',
+    xaxis_title='Fecha',
     yaxis_title='Eje Y',
     xaxis=dict(tickmode='array', tickvals=x, ticktext=x_labels),
     yaxis=dict(tickfont=dict(size=14)),
