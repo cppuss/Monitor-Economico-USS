e2191a4fe021a275bb487c0cf7a9eb69fed75253
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-06-15 13:31:28 -0400

    Update economic_dashboard.py

--- economic_dashboard.py
+++ economic_dashboard.py
@@ -148,20 +148,6 @@ def to_excel(df):
     processed_data = output.getvalue()
     return processed_data
 
-diccionario_meses = {
-    1: 'enero',
-    2: 'febrero',
-    3: 'marzo',
-    4: 'abril',
-    5: 'mayo',
-    6: 'junio',
-    7: 'julio',
-    8: 'agosto',
-    9: 'septiembre',
-    10: 'octubre',
-    11: 'noviembre',
-    12: 'diciembre'
-}
 
 data=pd.read_parquet(path+"datos_monitor.parquet")
 
