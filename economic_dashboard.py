a4c64f421d237550a22b6a0006f1b86171502587
Centro de Políticas Públicas USS <131295909+cpp-uss@users.noreply.github.com>
2023-06-28 18:02:55 -0400

    Update economic_dashboard.py

--- economic_dashboard.py
+++ economic_dashboard.py
@@ -110,7 +110,7 @@ def gen(imacec_des,rango,titulo):
         ))
 
     
-    def format_annotation(serie, periodo, valor):
+     def format_annotation(serie, periodo, valor):
         # Obtener el año y el mes de la fecha
        # fecha = datetime.strptime(periodo, "%Y-%m")
        # periodo_formateado = fecha.strftime("%Y-%m")  # Cambiar el formato según tus necesidades
