78cf8a295c0c37c4a64393354d70ec99f58f555d
cppuss <138695625+cppuss@users.noreply.github.com>
2023-08-08 15:46:13 -0400

    Update 2_TEST.py

--- pages/2_TEST.py
+++ pages/2_TEST.py
@@ -20,4 +20,4 @@ st.write(' ')
 data=pd.read_parquet("datos_monitor_1.parquet")
 data=data[data["CATEGORIA3"]=="NACIONAL"]
 st.dataframe(data)
-st.download_button("descargar", data, file_name=None)
+st.download_button("descargar", data, file_name="descargar.xlsx")
