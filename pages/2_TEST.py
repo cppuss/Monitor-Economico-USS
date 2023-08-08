769de903a23b208c777b1a0189cb415aeec14b94
cppuss <138695625+cppuss@users.noreply.github.com>
2023-08-08 15:41:23 -0400

    Update and rename 2_Generar Informe Económico to 2_TEST.py

---
+++ pages/2_TEST.py
@@ -0,0 +1,23 @@
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+from datetime import time
+from pptx import Presentation
+from io import BytesIO
+from datetime import date
+from pptx.dml.color import RGBColor
+from pptx.util import Inches
+import os
+
+st.markdown("<h1 style='text-align: center; color: black;'>Pruebas</h1>", unsafe_allow_html=True)
+
+
+
+st.write(' ')
+
+
+
+
+st.download_button(label, data, file_name=None,
