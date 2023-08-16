84a34feb65a6be893a9d477da67288b0937913ba
cppuss <138695625+cppuss@users.noreply.github.com>
2023-08-16 12:04:32 -0400

    Update 2_TEST.py

--- pages/2_TEST.py
+++ pages/2_TEST.py
@@ -27,7 +27,7 @@ from streamlit_extras.switch_page_button import switch_page
 
 want_to_contribute = streamlit.button("I want to contribute!")
 if want_to_contribute:
-    switch_page("generar presentacion")
+    switch_page("1_generar presentacion")
 
 
 
