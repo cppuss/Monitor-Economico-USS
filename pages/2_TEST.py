dd8a714a16ec94d04660684d34f20fcb81582ea4
cppuss <138695625+cppuss@users.noreply.github.com>
2023-08-16 11:57:04 -0400

    Update and rename 2_TEST to 2_TEST.py

---
+++ pages/2_TEST.py
@@ -0,0 +1,33 @@
+
+import streamlit 
+import pandas as pd
+import plotly.express as px
+import numpy as np
+from datetime import time
+from pptx import Presentation
+from io import BytesIO
+from datetime import date
+from pptx.dml.color import RGBColor
+from pptx.util import Inches
+import os
+
+st.markdown("<h1 style='text-align: center; color: black;'>Pruebas</h1>", unsafe_allow_html=True)
+
+
+
+st.write(' ')
+
+data=pd.read_parquet("datos_monitor_1.parquet")
+data=data[data["CATEGORIA3"]=="NACIONAL"]
+st.dataframe(data)
+
+
+
+from streamlit_extras.switch_page_button import switch_page
+
+want_to_contribute = st.button("I want to contribute!")
+if want_to_contribute:
+    switch_page("Generar Presentación")
+
+
+
