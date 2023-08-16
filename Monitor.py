0d401c43509101b89a277f3ee453f2095da44ebd
cppuss <138695625+cppuss@users.noreply.github.com>
2023-08-16 11:38:58 -0400

    Update Monitor.py

--- Monitor.py
+++ Monitor.py
@@ -154,6 +154,11 @@ def descargar_datos(data):
 
 data=pd.read_parquet("datos_monitor_1.parquet")
 
+from streamlit_extras.switch_page_button import switch_page
+
+cambiar_a_presentacion = st.button("Generar reporte automático")
+if cambiar_a_presentacion:
+    switch_page("1_Generar Presentación")
 
 tab1, tab2,tab3,tab4 = st.tabs(["ACTIVIDAD ECONÓMICA","INFLACIÓN","MERCADO LABORAL","CUENTAS CORRIENTES"])
 
